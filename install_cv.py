ddfc5a6cc4c1206e8f2cb509bf732abe7e353254
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-09 14:05:35 +0200

    Update install_cv.py

--- install_cv.py
+++ install_cv.py
@@ -5,3 +5,7 @@ sudo apt install pip
 
 sudo pip3 install opencv-contrib-python
 sudo pip3 install pyserial
+
+systemctl stop serial-getty@ttyS0.service
+
+echo "Remove the first two chunks from '/boot/cmdline.txt' to make communication work!"
