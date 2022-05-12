484bd42d2638028d877a7659974ce110bd13495d
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-12 14:51:00 +0200

    Add files via upload

---
+++ pig/pig.ino
@@ -0,0 +1,136 @@
+#include <Servo.h>
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *mleft = AFMS.getMotor(2);
+Adafruit_DCMotor *mright = AFMS.getMotor(1);
+
+int turnSpeed = 69;
+int forwardSpeed = 100;
+
+void setup() {
+  Serial.begin(9600);
+  AFMS.begin();
+  mleft->setSpeed(turnSpeed);
+  mright->setSpeed(turnSpeed);
+}
+
+void forward(int ms)
+{
+  mleft->setSpeed(forwardSpeed);
+  mright->setSpeed(forwardSpeed);
+  mleft->run(FORWARD);
+  mright->run(FORWARD);
+
+  delay(ms);
+}
+
+void backward(int ms)
+{
+  mleft->setSpeed(forwardSpeed);
+  mright->setSpeed(forwardSpeed);
+  mleft->run(BACKWARD);
+  mright->run(BACKWARD);
+
+  delay(ms);
+}
+
+void right(int ms)
+{
+  mleft->setSpeed(turnSpeed);
+  mright->setSpeed(turnSpeed);
+  mleft->run(FORWARD);
+  mright->run(BACKWARD);
+
+  delay(ms);
+}
+
+void left(int ms)
+{
+  mleft->setSpeed(turnSpeed);
+  mright->setSpeed(turnSpeed);
+  mleft->run(BACKWARD);
+  mright->run(FORWARD);
+
+  delay(ms);
+}
+
+void brake()
+{
+  mleft->setSpeed(RELEASE);
+  mright->setSpeed(RELEASE);
+}
+
+void loop() {
+  switch(Serial.parseInt())
+  {
+     case 1:
+      right(0);
+      break;
+
+     case 2:
+      left(0);
+      break;
+
+     case 3:
+      forward(0);
+      break;
+
+     case 4:
+      backward(0);
+
+     case 10:
+      forward(1000);
+      right(700);
+      forward(1000);
+      break;
+
+    case 20:
+      delay(500);
+      mleft->run(FORWARD);   //FORWARD
+      mright->run(FORWARD);
+      delay (1500);
+      brake();
+
+      delay(500);
+      mleft->run(BACKWARD);    //BACKWARD
+      mright->run(BACKWARD);
+      delay (1500);
+      brake();
+
+      delay(500);
+      mleft->run(RELEASE);   //LEFT CIRCLE
+      mright->run(FORWARD);
+      delay (1500);
+      brake();
+
+      delay(500);
+      backward(1500);
+      brake();
+      
+      delay(500);
+      mleft->setSpeed(0);
+      mright->setSpeed(250);
+      mleft->run(RELEASE);   //LEFT CIRCLE
+      mright->run(FORWARD);
+      delay (1500);
+      brake();
+
+      delay(2000);
+
+    case 30:
+      left(700);
+
+      forward(1000);
+
+      right(700);
+
+      brake();
+      break;
+
+    default:
+      brake();
+      break;
+  }
+}
